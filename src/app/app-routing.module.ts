3a94d262e889e40a89f1db6d7c1d9a686c80f026
Hichem <hichem.belala@lmdsio.fr>
2022-11-10 17:47:31 +0100

    Ratio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ListefichefraisComponent } from '../listefichefrais/listefichefrais.component';
 import { FichefraisComponent } from '../fichefrais/fichefrais.component';
-const routes: Routes = [];
+import { AjoutfichefraisComponent } from '../ajoutfichefrais/ajoutfichefrais.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { ConnexionComponent } from '../connexion/connexion.component';
+
+const routes: Routes = [
+  { path: '', redirectTo : '/connexion', pathMatch: 'full' },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'Listefichefrais', component: ListefichefraisComponent },
+  { path: 'modifierFrais/:id', component: FichefraisComponent },
+  { path: 'ajouterFrais', component: AjoutfichefraisComponent },
+  { path: 'accueil', component: NavbarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
