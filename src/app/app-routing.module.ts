2de8d7ae3242a7d37a276700f5e132c7f3c339fb
Hichem <hichem.belala@lmdsio.fr>
2022-11-10 16:23:16 +0100

    Ratio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CommonModule} from "@angular/common";
 import { RouterModule, Routes } from '@angular/router';
 
+import { ListefichefraisComponent } from '../listefichefrais/listefichefrais.component';
+import { FichefraisComponent } from '../fichefrais/fichefrais.component';
 const routes: Routes = [];
 
 @NgModule({
